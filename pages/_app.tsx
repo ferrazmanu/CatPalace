86d66c8b5b95afc3b68cc10828546adef42e4f33
ferrazmanu <ferraz.manuela@hotmail.com>
2023-01-23 17:59:29 -0300

    implementação script do google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,22 @@ import Theme from "../components/shared/Theme";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* Google tag (gtag.js) */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-FZD65B8WFY"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-FZD65B8WFY');
+              `,
+        }}
+      />
       <Theme>
         <GlobalStyle />
         <Header />
